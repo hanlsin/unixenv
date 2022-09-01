075ea7086d58c08ce7560cf258c5490378d3668c
YUN PARK <hanlsin@gmail.com>
2022-08-31 21:01:27 -0400

    fix git related cmd

--- scripts/conda_gotcha.sh
+++ scripts/conda_gotcha.sh
@@ -10,21 +10,20 @@
 [[ -z "${MACHINE_ARCH}" ]] && MACHINE_ARCH=$(source ${SCRIPT_DIR}/os_arch_gotcha.sh)
 
 # check conda home path
-[[ -z "${CONDA_HOME}" ]] && CONDA_HOME=${HOME}/opt/miniconda3
-[[ -z "${CONDA_HOME}" ]] && CONDA_HOME=${HOME}/miniconda3
-[[ -z "${CONDA_HOME}" ]] && CONDA_HOME=${HOME}/bin/miniconda3
+if [[ -z "${CONDA_HOME}" ]]; then
+    export CONDA_HOME=${HOME}/opt/miniconda3
+    export CONDA_EXEC=${CONDA_HOME}/condabin/conda
+fi
+[[ -z "${CONDA_HOME}" ]] && export CONDA_HOME=${HOME}/miniconda3
+[[ -z "${CONDA_HOME}" ]] && export CONDA_HOME=${HOME}/bin/miniconda3
 
 # check conda binary
-CONDA_EXEC=${CONDA_HOME}/bin/conda
+[[ -z "${CONDA_EXEC}" ]] && export CONDA_EXEC=${CONDA_HOME}/bin/conda
 ${CONDA_EXEC} > /dev/null
 if [ "$?" -eq 127 ]; then
     echo "Please install conda first."
     exit 127
 fi
 
-CONDA_ENV_NAME=palette
-CONDA_ENV_PATH=${CONDA_HOME}/envs/${CONDA_ENV_NAME}
-
-echo "* CONDA ENV"
-echo "  + Name: ${CONDA_ENV_NAME}"
-echo "  + Path: ${CONDA_ENV_PATH}"
+echo "* CONDA HOME: ${CONDA_HOME}"
+echo "* CONDA EXEC: ${CONDA_EXEC}"
